777bb7fb103321d0153784ca50208f1a56a70e34
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-01-18 20:00:05 -0300

    tercera tarea 2 parte

--- JS/hoja.js
+++ JS/hoja.js
@@ -6,13 +6,23 @@ function valornumero1(){
     let numero=document.getElementById("valor").value;
     if((numero<=15) || (numero=55) || (numero=75)){
         numerosuertudo=true
-        alert ("numero suertudo")
+        alert (parseFloat("numero suertudo"))
     }
     else{
         numeronormal=false
-        alert ("numero normales")
+        alert (parseFloat("numero normales"))
     }
   
 }
 
+/*SEGUNDO PROBLEMA*/
+var anoahorasiquesi= document.getElementById("ano")
+function valornumero(){
+    if  ((anoahorasiquesi.value % 4 == 0 && anoahorasiquesi.value %100 != 0 ) || (anoahorasiquesi.value % 400 == 0)){
+alert ("El año" + anoahorasiquesi.value + "es bisiesto")
+    }
+    else{
+alert ("El año" + anoahorasiquesi.value + "no es bisiesto")
+    }
+}
 
